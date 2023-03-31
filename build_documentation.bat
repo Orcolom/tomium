ca50806a493bc2a73b7565aee2d21cf5b6c742b5
Orcolom <joram.vdm@gmail.com>
2023-03-31 20:58:28 +0200

    finsihing my lazy docs pages

--- build_documentation.bat
+++ build_documentation.bat
@@ -1,4 +1,5 @@
 XCOPY "README.md" "./docfx_project/articles/intro.md" /Y
+XCOPY "README.md" "./docfx_project/index.md" /Y
 docfx docfx_project/docfx.json
 DEL ".\UnityProject-Tomium\Packages\com.orcolom.tomium\Documentation~\" /Q
 XCOPY "./docfx_project/_site" ".\UnityProject-Tomium\Packages\com.orcolom.tomium\Documentation~\" /Y /E
